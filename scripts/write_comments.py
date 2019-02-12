1b82b2656041685310e154de7571a5ea98f799fc
Julian Hammer <julian.hammer@fau.de>
2019-02-12 08:40:57 +0100

    better output and file processing order

--- scripts/write_comments.py
+++ scripts/write_comments.py
@@ -20,6 +20,7 @@ default_comment = load_comment(os.path.join(os.path.dirname(__file__), 'template
 
 def input_with_prefill(prompt, text):
     # from https://stackoverflow.com/a/8505387/2754040
+    # does not work in an interactive shell
     def hook():
         readline.insert_text(text)
     readline.set_startup_hook(hook)
@@ -47,78 +48,78 @@ def main():
         path_filter = ''
     path_filter = re.compile(path_filter)
 
+    # Compile list of files to process
+    files = []
     for glob_pattern in patterns_to_comment:
-        # TODO group by folder
-        for file_to_comment in glob(glob_pattern, recursive=True):
-            # Skip files based on filter
-            if not re.match(path_filter, file_to_comment):
-                continue
-            print('*', file_to_comment, end=': ')
+        files += list(filter(lambda x: re.match(path_filter, x), glob(glob_pattern, recursive=True)))
+    files.sort()
 
-            # Initialize comment with default data
-            comment = copy(default_comment)
+    print('Selected files:', len(files))
+    print()
 
-            # Load already existing comment
-            comment_file = file_to_comment+'.comment.yml'
-            try:
-                comment.update(load_comment(comment_file))
-            except FileNotFoundError:
-                pass
-            except TypeError:
-                print('malformed comment file, will overwrite with defaults.')
+    for n, file_to_comment in enumerate(files):
+        print('{:>4}/{:<4} '.format(n+1, len(files)), file_to_comment)
+        print('Status: ', end='')
 
-            # Check if it is up-to-date
-            try:
-                latest_commit_hash = get_latest_commit_hash(file_to_comment)
-            except CalledProcessError:
-                print('does not exist or is not in a git repo.')
-                continue
-            if latest_commit_hash == '':
-                print('has not been commited. Commit first and than comment.')
-                continue
-            if comment['commithash']:
-                if latest_commit_hash.startswith(comment['commithash']):
-                    print('comment and review is up-to-date. You may update it manually.')
-                    continue
+        # Initialize comment with default data
+        comment = copy(default_comment)
 
-            # Ask user to update comment and review
-            print('needs review and (optional) comment')
-            # commithash
-            print('{}: {} -> {} (latest commit hash)'.format(
-                'commithash', comment['commithash'],latest_commit_hash))
-            comment['commithash'] = latest_commit_hash
-            # review
-            try:
-                first = True
-                valid_inputs = ['red', 'orange', 'green', 'grey', None]
-                while first or comment['review'] not in valid_inputs:
-                    if not first:
-                        print('Valid inputs are: {}'.format(valid_inputs))
-                    else:
-                        first = False
-                    comment['review'] = input_with_prefill('review: ', comment['review']) or None
-            except EOFError:
-                print(' [skipped]\n')
-                continue
-            # comment
-            try:
-                comment['comment'] = input_with_prefill('comment: ', comment['comment']) or None
-            except EOFError:
-                print(' [skipped]\n')
+        # Load already existing comment
+        comment_file = file_to_comment+'.comment.yml'
+        try:
+            comment.update(load_comment(comment_file))
+        except FileNotFoundError:
+            pass
+        except TypeError:
+            print('malformed comment file, will overwrite with defaults.')
+
+        # Check if it is up-to-date
+        try:
+            latest_commit_hash = get_latest_commit_hash(file_to_comment)
+        except CalledProcessError:
+            print('does not exist or is not in a git repo.')
+            continue
+        if latest_commit_hash == '':
+            print('has not been commited. Commit first and than comment.')
+            continue
+        if comment['commithash']:
+            if latest_commit_hash.startswith(comment['commithash']):
+                print('comment and review is up-to-date. You may update it manually.')
                 continue
 
-            # save and show author
-            if author:
-                print('auther: {}'.format(author))
-                comment['author'] = author
-            with open(comment_file, 'w') as f:
-                yaml.dump(comment, f)
-            print('saved to {}'.format(comment_file))
-            print()
+        # Ask user to update comment and review
+        print('needs review and (optional) comment')
+        # commithash
+        print('{}: {} -> {} (latest commit hash)'.format(
+            'commithash', comment['commithash'],latest_commit_hash))
+        # review
+        try:
+            first = True
+            valid_inputs = ['red', 'orange', 'green', 'grey', None]
+            while first or comment['review'] not in valid_inputs:
+                if not first:
+                    print('Valid inputs are: {}'.format(valid_inputs))
+                else:
+                    first = False
+                comment['review'] = input_with_prefill('review: ', comment['review']) or None
+        except EOFError:
+            print(' [skipped]\n')
+            continue
+        # comment
+        try:
+            comment['comment'] = input_with_prefill('comment: ', comment['comment']) or None
+        except EOFError:
+            print(' [skipped]\n')
+            continue
 
-
-
-
+        # save and show author
+        if author:
+            print('auther: {}'.format(author))
+            comment['author'] = author
+        with open(comment_file, 'w') as f:
+            yaml.dump(comment, f)
+        print('saved to {}'.format(comment_file))
+        print()
 
 if __name__ == '__main__':
     main()
