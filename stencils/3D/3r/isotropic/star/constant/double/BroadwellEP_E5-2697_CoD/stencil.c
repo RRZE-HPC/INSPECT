c5f9dbe3d18242ff7af14c798f0a96eddd0fa2f5
Julian Hornich <julian.hornich@fau.de>
2019-03-24 23:21:52 +0100

    UpÃdate 3D/3r/isotropic/star/constant/double/BroadwellEP_E5-2697_CoD

--- stencils/3D/3r/isotropic/star/constant/double/BroadwellEP_E5-2697_CoD/stencil.c
+++ stencils/3D/3r/isotropic/star/constant/double/BroadwellEP_E5-2697_CoD/stencil.c
@@ -1,19 +1,18 @@
 double a[M][N][P];
 double b[M][N][P];
 double c0;
+double c1;
+double c2;
+double c3;
 
-for ( int k = 3; k < M - 3; k++ )
-{
-	for ( int j = 3; j < N - 3; j++ )
-	{
-		for ( int i = 3; i < P - 3; i++ )
-		{
-			b[k][j][i] =
-			  c0
-			  * ( a[k][j][i] + a[k][j][i - 1] + a[k][j][i + 1] + a[k - 1][j][i] + a[k + 1][j][i]
-			      + a[k][j - 1][i] + a[k][j + 1][i] + a[k][j][i - 2] + a[k][j][i + 2] + a[k - 2][j][i]
-			      + a[k + 2][j][i] + a[k][j - 2][i] + a[k][j + 2][i] + a[k][j][i - 3] + a[k][j][i + 3]
-			      + a[k - 3][j][i] + a[k + 3][j][i] + a[k][j - 3][i] + a[k][j + 3][i] );
-		}
-	}
+for(long k=3; k < M-3; ++k){
+for(long j=3; j < N-3; ++j){
+for(long i=3; i < P-3; ++i){
+b[k][j][i] = c0 * a[k][j][i]
++ c1 * ((a[k][j][i-1] + a[k][j][i+1]) + (a[k-1][j][i] + a[k+1][j][i]) + (a[k][j-1][i] + a[k][j+1][i]))
++ c2 * ((a[k][j][i-2] + a[k][j][i+2]) + (a[k-2][j][i] + a[k+2][j][i]) + (a[k][j-2][i] + a[k][j+2][i]))
++ c3 * ((a[k][j][i-3] + a[k][j][i+3]) + (a[k-3][j][i] + a[k+3][j][i]) + (a[k][j-3][i] + a[k][j+3][i]))
+;
+}
+}
 }
