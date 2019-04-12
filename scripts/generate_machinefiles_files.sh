753b3e42b1f2a1f6f4641c96bcbea18213351c71
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-12 13:48:14 +0000

    Deploy RRZE-HPC/INSPECT to github.com/RRZE-HPC/INSPECT.git:gh-pages

--- scripts/generate_machinefiles_files.sh
+++ scripts/generate_machinefiles_files.sh
@@ -6,6 +6,7 @@ for file in ./_data/machine_files/*yml; do
 	if [[ $file != *"comment"* ]]; then
 		machine=$(echo $file | sed 's/\.yml//; s/.*\///')
 		echo '---' > machines/${machine}.md
+		echo "machine: ${machine}" >> machines/${machine}.md
 		echo '---' >> machines/${machine}.md
 		echo '' >> machines/${machine}.md
 		echo "{% include template_machinefile.md file=site.data.machine_files.${machine} %}" >> machines/${machine}.md
